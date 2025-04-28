6610287179b8257b52f389c6388551281d921d0c
WahidzSzz <135294842+WahidzSzz@users.noreply.github.com>
2025-04-28 17:08:12 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,29 +1,33 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/WahidzSzz/WahidzSzz/refs/heads/main/SympleLibrary.lua"))()
 
 Library:AddWindow({
-	Title = "library",
-	Description = "whid",
-	Icon = "rbz"
-	
+	Title = "Title Library",
+	Description = "openbutton name",
+	Icon = "icon open button"
 })
 
+
 Library:AddSection({
-	Title = "section",
-	Color3 = 255, 255, 255
+	Title = "Section",
+	TextColor = Color3.fromRGB(84, 175, 255)
+})
+
+Library:AddButton({
+	Title = "Only A Button",
+	Callback = function()
+		Library:Notification({
+			Title = "Notification",
+			Desc = "Description",
+			Duration = "10"
+		})
+	end
 })
 
 Library:AddToggle({
 	Title = "Toggle",
 	Callback = function()
-		print("called")
-	end,
-})
-
-Library:AddButton({
-	Title = "button",
-	Callback = function()
-		print("buttoncalled")
-	end,
+		print("toggle")
+	end
 })
 
 --- no tab because its so simple fr
